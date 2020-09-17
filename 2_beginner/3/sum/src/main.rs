762c707f0ff425e90d08325072978908780f096d
DenTaku <dentaku1412@gmail.com>
2020-09-17 23:35:41 +0900

    Learn DP in the middle via partial sum

---
+++ 2_beginner/3/sum/src/main.rs
@@ -0,0 +1,7 @@
+use competitive::prelude::*;
+
+#[argio(output = AtCoder)]
+fn main(n: usize, a: [i64; n], m: [usize; n], K: i64) -> bool {
+    let mut dp = vec![-1; (n + 1) * (m + 1)];
+    false
+}
