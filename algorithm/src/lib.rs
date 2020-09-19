8c92f04fb592b601e72c0b6964c076b57b468441
DenTaku <dentaku1412@gmail.com>
2020-09-20 07:50:58 +0900

    Add UnionFind

--- algorithm/src/lib.rs
+++ algorithm/src/lib.rs
@@ -156,6 +156,56 @@ impl<T> BinaryHeap<T>
 
 // end BinaryHeap ********************************************* 
 
+// UnionFind **************************************************
+struct UnionFind {
+    par: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        UnionFind {
+            par: { 
+                let mut v = Vec::with_capacity(n);
+                for i in 0..n {
+                    v.push(i);
+                }
+                v
+            },
+            rank: vec![0;n]
+        }
+    }
+
+    fn find(&self, x: usize) -> usize {
+        if self.par[x] == x {
+            x
+        } else {
+            self.find(self.par[x])
+        }
+    }
+    
+    fn unite(&mut self, x: usize, y: usize) {
+        let x_par = self.find(x);
+        let y_par = self.find(y);
+        if x_par == y_par {
+            return ();
+        }
+        if self.rank[x_par] < self.rank[y_par] {
+            self.par[x_par] = y_par;
+        } else {
+            self.par[y_par] = x_par;
+            if self.rank[x_par] == self.rank[y_par] {
+                self.rank[x_par] += 1;
+            }
+        }
+    }
+
+    fn same(&self, x:usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+}
+
+// end UnionFind ****************************************
 
 
 #[cfg(test)]
